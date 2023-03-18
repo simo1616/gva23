762cf5ed9522f22286a3774fd5e24919bcb5a262
simo1616 <83559414+simo1616@users.noreply.github.com>
2023-03-18 18:12:02 +0100

    first commit

---
+++ src/main/java/com/gva23/volontaires/Gva23Application.java
@@ -0,0 +1,13 @@
+package com.gva23.volontaires;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gva23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gva23Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gva23/volontaires/Gva23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gva23.volontaires;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gva23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
